f05a292630f00be8601c365f33846f52b654b5f6
zakariyalaswad <116490990+zakariyalaswad@users.noreply.github.com>
2025-01-24 22:39:31 +0100

    modify profil lien

--- src/pages/profil.js
+++ src/pages/profil.js
@@ -80,7 +80,7 @@ function Profil() {
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><i className="bi bi-person-circle" title='Profil'></i></a>
+                    <Link className="nav-link" to="/profil"><i className="bi bi-person-circle" title='Profil'></i></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
