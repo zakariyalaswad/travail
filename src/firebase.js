4c2f690b9347e766982ac445fab9b21a00b5641f
zakariyalaswad <116490990+zakariyalaswad@users.noreply.github.com>
2025-02-19 17:10:14 +0000

    changement de base

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { initializeAuth, browserLocalPersistence } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDN41JWsp08ygrVHFk-8kza3xqcu8d47AU",
-  authDomain: "travail-d8e57.firebaseapp.com",
-  databaseURL: "https://travail-d8e57-default-rtdb.firebaseio.com",
-  projectId: "travail-d8e57",
-  storageBucket: "travail-d8e57.firebasestorage.app",
-  messagingSenderId: "591570363",
-  appId: "1:591570363:web:7162b3f2a7ab40d6fbd09e",
-  measurementId: "G-CVQRF717LL"
+  apiKey: "AIzaSyA-FgO27atz_m4E01851bwEPt3sSM_YQrY",
+  authDomain: "tache-collaboratif.firebaseapp.com",
+  projectId: "tache-collaboratif",
+  storageBucket: "tache-collaboratif.firebasestorage.app",
+  messagingSenderId: "878781911993",
+  appId: "1:878781911993:web:abc6a8bd040902c54c5c68",
+  measurementId: "G-RC2K1MJ4V0"
 };
 
 // Initialize Firebase
